60ceb70b3add05d8e862f18d1ce585b564ff35bc
Shlok Gandhi <shlok.gandhi@gmail.com>
2016-02-27 16:38:55 +0530

    Micropost layout added

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,6 @@ User.create!(name:  "Raj Gandhi",
 	email: "raj.gandhi@mail.com",
 	password:              "barfoo",
 	password_confirmation: "barfoo",
-	admin: true,
 	activated: true,
 	activated_at: Time.zone.now)
 
@@ -31,4 +30,10 @@ User.create!(name:  "Raj Gandhi",
 		password_confirmation: password,
 		activated: true,
 		activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+	content = Faker::Lorem.sentence(5)
+	users.each { |user| user.microposts.create!(content: content)}
 end
