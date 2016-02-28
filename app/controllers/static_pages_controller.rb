e370fd526121003ce14856cc335e3d3fa3d2c623
Shlok Gandhi <shlok.gandhi@gmail.com>
2016-02-28 13:23:34 +0530

    Done with adding microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,18 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+	def home
+		if logged_in?
+			@micropost = current_user.microposts.build
+			@feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+		end
+	end
 
-  def help
-  end
+	def help
+	end
 
-  def about
-  end
+	def about
+	end
 
-  def contact
-  end
+	def contact
+	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,10 @@ class User < ActiveRecord::Base
 	def password_reset_expired?
 		reset_sent_at < 2.hours.ago
 	end
+
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
 	
 	private
 
