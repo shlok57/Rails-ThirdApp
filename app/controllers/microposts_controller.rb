9050e7d0611ee6ca81a6355d5bc3b4a8274a6099
Shlok Gandhi <shlok.gandhi@gmail.com>
2016-03-01 14:46:13 +0530

    Added image uploads

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -23,9 +23,9 @@ class MicropostsController < ApplicationController
 	private
 
 	def micropost_params
-		params.require(:micropost).permit(:content)
-	end
-
+      params.require(:micropost).permit(:content, :picture)
+    end
+    
 	def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -30,7 +30,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       delete micropost_path(first_micropost)
     end
     # Visit a different user.
-    get user_path(users(:archer))
+    get user_path(users(:raj))
     assert_select 'a', text: 'delete', count: 0
   end
 end
